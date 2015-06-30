50a7fd03fef6eebfc900d70597f44c33e56e87b8
sajattack <sajattack@gmail.com>
2015-06-29 18:05:10 -0700

    add everything to the github repo

---
+++ app/src/main/java/com/paulsajna/mainmenuadapter/MainActivity.java
@@ -0,0 +1,36 @@
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+		String[] titles = getResources().getStringArray(R.array.menu_titles);
+		String[] descs = getResources().getStringArray(R.array.menu_descs);
+		String[] classes = getResources().getStringArray(R.array.menu_classes);
+				
+		TypedArray icons = getResources().obtainTypedArray(R.array.menu_icons);		
+		setListAdapter(new MainMenuAdapter(this, R.layout.main_list_item,
+				titles, descs, icons, classes));
+    }
+	
+	public void onListItemClick(ListView parent, View v, int position, long id) {
+		String StringClassname = parent.getAdapter().getItem(position).toString();
+		Class<?> c = null;
+        if(StringClassname != null) {
+            try {
+                c = Class.forName(StringClassname);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
+	}
+}
---
+++ app/src/main/java/com/paulsajna/mainmenuadapter/MainMenuAdapter.java
@@ -0,0 +1,68 @@
+package com.sajnasoft.nettoolbox;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainMenuAdapter extends ArrayAdapter<String> {
+
+	private LayoutInflater mInflater;
+	
+	private String[] mTitles;
+	private String[] mDescs;
+	private TypedArray mIcons;
+	private String[] mClasses;
+	
+	private int mViewResourceId;
+	
+	public MainMenuAdapter(Context ctx, int viewResourceId,
+			String[] titles, String[] descs, TypedArray icons, String[] classes) {
+		super(ctx, viewResourceId, titles);
+		
+		mInflater = (LayoutInflater)ctx.getSystemService(
+				Context.LAYOUT_INFLATER_SERVICE);
+		
+		mTitles = titles;
+		mDescs = descs;
+		mIcons = icons;
+		mClasses = classes;
+		
+		mViewResourceId = viewResourceId;
+	}
+
+	@Override
+	public int getCount() {
+		return mTitles.length;
+	}
+
+	@Override
+	public String getItem(int position) {
+		return mClasses[position];
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		convertView = mInflater.inflate(mViewResourceId, null);
+		
+		ImageView iv = (ImageView)convertView.findViewById(R.id.item_icon);
+		iv.setImageDrawable(mIcons.getDrawable(position));
+
+		TextView tv = (TextView)convertView.findViewById(R.id.item_title);
+		tv.setText(mTitles[position]);
+		
+		TextView tv2 = (TextView)convertView.findViewById(R.id.item_desc);
+		tv2.setText(mDescs[position]);
+		
+		return convertView;
+	}
+}
